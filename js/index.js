525d901d22d9da8c4acd0ae025409bba3f8dfec1
Kurt Van Etten <kurt@vanetten.us>
2019-08-11 21:04:41 -0400

    Change nav links when we hover over them

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 // Your code goes here
 
+
+// have our nav links change when we hover over them
+// (yes, we could have done this just by using css!)
+const navEnter = event => {
+	event.target.classList.add('hover');
+};
+
+const navLeave = event => {
+	event.target.classList.remove('hover');
+};
+
+const navList = document.querySelectorAll('.nav-link');
+
+navList.forEach(element => {
+	element.addEventListener('mouseenter', navEnter);
+	element.addEventListener('mouseleave', navLeave);
+});
+
